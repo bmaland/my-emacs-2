a3c4b60c98b31f01ff74384c8ba7eb31ae87fef3
Bjørn Arild Mæland <bjorn.maeland@gmail.com>
2013-10-29 14:23:43 +0100

    Lock to magit 1.1.2

--- bjorn/my-packages.el
+++ bjorn/my-packages.el
@@ -21,6 +21,16 @@
         (:name pretty-symbols
                :type git
                :url "git://github.com/drothlis/pretty-symbols.git")
+        (:name magit
+               :website "https://github.com/magit/magit#readme"
+               :description "It's Magit! An Emacs mode for Git."
+               :type github
+               :pkgname "magit/magit"
+               :branch "1.1.2"
+               :info "."
+               :autoloads ("50magit")
+               :build (("make" "all"))
+               :build/darwin `(,(concat "make EMACS=" el-get-emacs " all")))
         ))
 
 (setq my-packages
@@ -39,6 +49,7 @@
     how-do-i
     pretty-symbols
     rhtml-mode
+    magit
     ))
 
 (el-get 'sync my-packages)
