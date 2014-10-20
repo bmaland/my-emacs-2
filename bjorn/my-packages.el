616ef358d2f1065c344d6f095de728b28efa3df9
Bjørn Mæland <bjorn@bmaland.com>
2014-10-20 20:53:24 +0200

    Update el-get install

--- bjorn/my-packages.el
+++ bjorn/my-packages.el
@@ -1,12 +1,12 @@
 (add-to-list 'load-path "~/.emacs.d/el-get/el-get")
 
 ;; make sure el-get is installed
-(unless (require 'el-get nil t)
-  (url-retrieve
-   "https://raw.github.com/dimitri/el-get/master/el-get-install.el"
-   (lambda (s)
-     (goto-char (point-max))
-     (eval-print-last-sexp))))
+(unless (require 'el-get nil 'noerror)
+  (with-current-buffer
+      (url-retrieve-synchronously
+       "https://raw.githubusercontent.com/dimitri/el-get/master/el-get-install.el")
+    (goto-char (point-max))
+    (eval-print-last-sexp)))
 
 (setq el-get-sources
       '(
