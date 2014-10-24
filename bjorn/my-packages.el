aa4bafdc91cc1ba8d83b30283bafc352ed435998
Bjørn Mæland <Bjorn.Maeland@groupm.com>
2014-10-24 12:30:19 +0200

    el-get-user recipes

--- bjorn/my-packages.el
+++ bjorn/my-packages.el
@@ -5,44 +5,21 @@
 
 (require 'el-get)
 
-(setq el-get-sources
-      '(
-        (:name liquid-mode
-               :type git
-               :url "git://github.com/rehanift/Emacs-Liquid.git"
-               :post-init (progn
-                            (autoload 'liquid-mode "liquid" nil t)))
-        (:name how-do-i
-               :type git
-               :url "git://github.com/edmellum/how-do-i.el.git")
-        (:name pretty-symbols
-               :type git
-               :url "git://github.com/drothlis/pretty-symbols.git")
-        (:name magit
-               :website "https://github.com/magit/magit#readme"
-               :description "It's Magit! An Emacs mode for Git."
-               :type github
-               :pkgname "magit/magit"
-               :branch "1.1.2"
-               :info "."
-               :autoloads ("50magit")
-               :build (("make" "all"))
-               :build/darwin `(,(concat "make EMACS=" el-get-emacs " all")))
-        ))
+(add-to-list 'el-get-recipe-path "~/.emacs.d/el-get-user/recipes")
 
 (setq my-packages
-  '(
-    anything
-    expand-region
-    golden-ratio
-    smex
-    textmate
-    tomorrow-theme
-    ;; yasnippet
-    ;; pretty-symbols
-    magit
-    ;; git-timemachine
-    ))
+      '(
+        anything
+        expand-region
+        golden-ratio
+        smex
+        textmate
+        tomorrow-theme
+        yasnippet
+        pretty-symbols
+        magit
+        git-timemachine
+        ))
 
 (el-get 'sync my-packages)
 (provide' my-packages)
